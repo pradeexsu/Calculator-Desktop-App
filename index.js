f2f24e668ab7ea37faa416416503a01d5b3ff21b
Pradeep Suthar <sutharp777@gmail.com>
2021-01-02 21:25:41 +0530

    code formatting

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow} = require('electron');
 const url = require('url');
 var ipc = require('electron').ipcMain;
-
-
 let win = null;
-
-
 function boot() {
-	win = new BrowserWindow({
-		height:310,
-		width:280,
-		frame: false,
-		webPreferences: {
-			nodeIntegration: true
-		},
-		resizable: false,
-		transparent: true,
-		icon: __dirname + '/ico.png'
-	})
-	win.loadURL(`file://${__dirname}/index.html`)
-	win.on('closed',()=>{
-		win = null;
-	})
-	ipc.on('invokeAction', function(event, data){
-		win.minimize();
-	})
-
+    win = new BrowserWindow({
+        height: 310,
+        width: 280,
+        frame: false,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        resizable: false,
+        transparent: true,
+        icon: __dirname + '/ico.png'
+    })
+    win.loadURL(`file: //${__dirname}/index.html`)
+        win.on('closed', () = > {
+            win = null;
+        }) ipc.on('invokeAction', function(event, data) {
+            win.minimize();
+        })
+        // const { webFrame } = require('electron').webFrame
+        // webFrame.setZoomFactor(1)
+        // webFrame.setVisualZoomLevelLimits(1, 1)
 }
 app.on('ready', boot);
